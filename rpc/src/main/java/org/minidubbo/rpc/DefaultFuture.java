54986ba9bad00775ad15f137fbb6f1f7d296a6b5
weishia <416664148@qq.com>
2023-01-15 00:38:42 +0800

    时间轮工具类

--- rpc/src/main/java/org/minidubbo/rpc/DefaultFuture.java
+++ rpc/src/main/java/org/minidubbo/rpc/DefaultFuture.java
@@ -1,6 +1,10 @@
 package org.minidubbo.rpc;
 
 import io.netty.channel.Channel;
+import org.minidubbo.rpc.timer.HashedWheelTimer;
+import org.minidubbo.rpc.timer.Timeout;
+import org.minidubbo.rpc.timer.Timer;
+import org.minidubbo.rpc.timer.TimerTask;
 
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
@@ -13,11 +17,16 @@ public class DefaultFuture extends CompletableFuture {
 
     private Request request;
 
+    private static final Timer TIMER = new HashedWheelTimer();
+
     private static final Map<Long/*requestId*/, CompletableFuture> FUTURE_MAP = new ConcurrentHashMap<>();
 
     public DefaultFuture(Channel channel,Request request){
         this.channel = channel;
         this.request = request;
         FUTURE_MAP.put(request.getId(),this);
+
     }
+
+
 }
